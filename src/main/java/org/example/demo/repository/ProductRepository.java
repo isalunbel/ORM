a968fb65d5a6210f68f574cb23151e8d9a3a05b2
isalunbel <142603867+isalunbel@users.noreply.github.com>
2024-05-23 08:08:09 +0300

    Update ProductRepository.java

--- src/main/java/org/example/demo/repository/ProductRepository.java
+++ src/main/java/org/example/demo/repository/ProductRepository.java
@@ -1,27 +1,16 @@
 package org.example.demo.repository;
 
 import org.example.demo.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
-public class ProductRepository {
+public interface ProductRepository extends JpaRepository<Order, Long> {
 
-    @PersistenceContext
-    private final EntityManager entityManager;
-
-    public ProductRepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
-
-    public List<Order> getOrdersByCustomerName(String name) {
-        String jpql = "SELECT o FROM Order o JOIN o.customer c WHERE LOWER(c.name) = LOWER(:name)";
-        TypedQuery<Order> query = entityManager.createQuery(jpql, Order.class);
-        query.setParameter("name", name);
-        return query.getResultList();
-    }
+    @Query("SELECT o FROM Order o JOIN o.customer c WHERE LOWER(c.name) = LOWER(:name)")
+    List<Order> getOrdersByCustomerName(@Param("name") String name);
 }
